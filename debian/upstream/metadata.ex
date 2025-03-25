ea50caf77e369d183ce1ad6075fd868e84853aa4
joe <joe@rstrike.my.id>
2025-03-25 16:32:13 +0000

    Initial commit

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/sherlock/issues
+# Bug-Submit: https://github.com/<user>/sherlock/issues/new
+# Changelog: https://github.com/<user>/sherlock/blob/master/CHANGES
+# Documentation: https://github.com/<user>/sherlock/wiki
+# Repository-Browse: https://github.com/<user>/sherlock
+# Repository: https://github.com/<user>/sherlock.git
